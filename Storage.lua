261456533d94e96ab51701fe0ce334768430ac63
Ruscios <toddjr@bertels.net>
2019-12-23 09:02:54 -0800

    Create Storage.lua
    Initial file structure. Full file needs to be grabbed from original minecraft server

---
+++ Storage.lua
@@ -0,0 +1,16 @@
+--API Paths--
+local com = require('component')
+local rs = com.block_refinedstorage_interface
+local event = require('event')
+
+--Local Variables--
+local amount
+local aDiff
+local noConflict
+
+--Stock Tables--
+local stock = {
+  {"minecraft:iron_ingot", 0.0, 64}
+}
+
+--Main--
