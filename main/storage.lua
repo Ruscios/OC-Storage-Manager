6a2995ce02854fb1acf70cf70a93c5ce56b6f8fa
Ruscios <toddjr@bertels.net>
2019-12-26 21:10:38 -0800

    Update storage.lua
    Created function conflictCheck from code originally in Main. Also changed program exit from an event pull for touch, to an event listen for key press.

--- main/storage.lua
+++ main/storage.lua
@@ -5,7 +5,6 @@ local event = require("event")
 
 --Variable--
 local aDiff
-local noConflict
 local item
 local taskDamage
 local taskName
@@ -16,33 +15,34 @@ stock = {
 --  {"minecraft:torch", 0.0, 8}
 }
 
+--Task Conflict Check--
+function conflictCheck (itemName, itemDamage)
+  local checkConflict = 1.0
+  for j, w in ipairs(rs.getTasks()) do
+    taskName = w.stack.item.name
+    taskDamage = w.stack.item.damage
+    if taskName == itemName then
+      if taskDamage == itemDamage then
+        checkConflict = nil
+      end
+    end
+  return checkConflict
+end
+
 --Main--
-
 while 1 do
-
+    
+event.listen(key_down(),os.exit())
 for i, v in pairs(stock) do
   if rs.getItem({name=v[1], damage=v[2]}) then
     item = rs.getItem({name=v[1], damage=v[2]})
     aDiff = v[3] - item.size
     if aDiff>0 then
-      noConflict = 1.0
-      for j, w in ipairs(rs.getTasks()) do
-        taskName = w.stack.item.name
-        taskDamage = w.stack.item.damage
-        if taskName == v[1] then
-          if taskDamage == v[2] then
-            noConflict = nil
-          end
-        end
-      end
-      if noConflict then
+      if conflictCheck(v[1], v[2]) then
         rs.scheduleTask({name=v[1], damage=v[2]}, aDiff)
       end
     end
   end
-end
-if event.pull(1,"touch") then
-  os.exit()
 end
 
 end
